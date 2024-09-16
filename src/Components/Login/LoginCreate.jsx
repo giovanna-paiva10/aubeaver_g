28d0da7b01dcd74f8cd0b722a73772f61ddb5885
Gabriel H. <gabrielhebert68@gmail.com>
2024-09-15 22:18:12 -0300

    socorro deus

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -17,7 +17,7 @@ const LoginCreate = () => {
 
     function handleSubmit(event) {
         event.preventDefault();
-        console.log('cadastrado');
+        console.log(messageRef.current.value);
       }
 
 
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getfirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDI2Ra6xg7pbvt2RLmHZt0AgWQoXmygfxo",
+  authDomain: "aubeaver-8aa8a.firebaseapp.com",
+  projectId: "aubeaver-8aa8a",
+  storageBucket: "aubeaver-8aa8a.appspot.com",
+  messagingSenderId: "229985108021",
+  appId: "1:229985108021:web:ae7f78866760da7a72b17c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getfirestore(app)
