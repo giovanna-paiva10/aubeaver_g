f7a4d1851774fe9b1c01a7fa5c05c0967d3e45f6
alexsilva21 <stargreen147@gmail.com>
2024-09-19 10:51:56 -0300

    footer e menu

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,39 +2,32 @@ import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
-
     return (
-     <> 
-<div>
-        <footer>
+        <footer className={styles.footer}>
             <nav className={styles.navfot}>
-       <ul className={styles.navLinks}>
-        <li>
-            <a href="/Termos">Termos</a>
-       </li>
-       </ul>
-       <ul className={styles.navLinks}>
-        <li>
-        <a href="/Contato">Contato</a>
-        </li>
-       </ul>
-       <ul className={styles.navLinks}>
-        <li>
-        <a href="/Feedbacks">Feedbacks</a>
-       </li>
-       </ul>
-       <ul className={styles.navLinks}>
-        <li>
-        <a href="/Sociais">Sociais</a>
-        </li>
-       </ul>
-    </nav>
+                <ul className={styles.navLinks}>
+                    <li>
+                        <a href="termos">Termos</a>
+                    </li>
+                </ul>
+                <ul className={styles.navLinks}>
+                    <li>
+                        <a href="contato">Contato</a>
+                    </li>
+                </ul>
+                <ul className={styles.navLinks}>
+                    <li>
+                        <a href="feedbacks">Feedbacks</a>
+                    </li>
+                </ul>
+                <ul className={styles.navLinks}>
+                    <li>
+                        <a href="sociais">Sociais</a>
+                    </li>
+                </ul>
+            </nav>
         </footer>
-
-</div>
-
-     </>
-    )
+    );
 };
 
 export default Footer;
