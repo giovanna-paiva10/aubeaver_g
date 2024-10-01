f947eebf38082fa912c418f56e9b06c4ef41699f
giovanna-paiva10 <giovannadeandrade0507@gmail.com>
2024-10-01 12:45:37 -0300

    home css

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -45,22 +45,22 @@ const Home = () =>{
                 <div className={styles.content1}>
                     <div> <img className={styles.imgg} src={garf} alt="garfield" /> 
                     <div> 
-                    <h4>Doação de alimentos</h4>
-                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras diam sem, tristique vel </h5>
+                    <h3 className={styles.contenttitle}>Doação de alimentos</h3>
+                    <h4 className={styles.contenttext}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras diam sem, tristique vel interdum sit amet, porta vel nibh. </h4>
                 </div></div></div>
 
                 <div className={styles.content1}>
                     <div> <img className={styles.imgg}src={garf} alt="garfield" /> 
                     <div> 
-                    <h4>Doação de alimentos</h4>
-                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras diam sem, tristique vel interdum sit amet, porta vel nibh. </h5>
+                    <h3 className={styles.contenttitle}>Doação de alimentos</h3>
+                    <h4 className={styles.contenttext}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras diam sem, tristique vel interdum sit amet, porta vel nibh. </h4>
                 </div></div></div>
 
                 <div className={styles.content1}>
                     <div> <img className={styles.imgg} src={garf} alt="garfield" /> 
                     <div> 
-                    <h4>Doação de alimentos</h4>
-                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras diam sem, tristique vel interdum sit amet, porta vel nibh. </h5>
+                    <h3 className={styles.contenttitle}>Doação de alimentos</h3>
+                    <h4 className={styles.contenttext}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras diam sem, tristique vel interdum sit amet, </h4>
                 </div></div></div>
 
         </div>
@@ -80,12 +80,12 @@ const Home = () =>{
                 <h4 className={styles.texto}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel malesuada tellus. Mauris ornare maximus libero, ut tempor odio tempus eu. Nam sed risus rhoncus, hendrerit ex nec, mattis ante. Morbi accumsan ac risus a scelerisque. Maecenas quis ligula vel diam finibus cursus nec eu elit. Donec ex tellus, facilisis vitae mi eget, malesuada malesuada diam. Praesent porttitor risus convallis tellus efficitur euismod. Vestibulum iaculis orci eget odio pharetra, id dignissim dui pharetra. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla metus urna, venenatis sit amet nulla eu, mattis mollis ligula.
 
                 Curabitur viverra justo a augue commodo volutpat. Curabitur id leo tortor. Donec vel pharetra purus. Maecenas et varius ipsum. Etiam aliquet neque nisl, ornare pellentesque lacus tristique at. Donec dictum mollis ante, a vestibulum arcu pellentesque sit amet. Nulla tempus tellus ac mi pellentesque, rhoncus dignissim elit tincidunt.</h4>
-            <div> 
+                <div>
                 <img className={styles.imggg} src={fotoo} alt="foto" />
-            </div>
+                </div>
             </div>
         </div>
-
+    
     </div>
     
 </body>
