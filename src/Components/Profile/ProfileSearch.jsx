757953561fd5f70b1396e3cb67e8c4742b6d4d7d
giovannadeandrade0507@gmail.com <giovannadeandrade0507@gami.com>
2024-10-08 12:45:42 -0300

    pag pesquisa css

--- src/Components/Profile/ProfileSearch.jsx
+++ src/Components/Profile/ProfileSearch.jsx
@@ -8,27 +8,44 @@ const ProfileSearch = () => {
 
     return (
         <>
-         <body id={StyleSheet.borda}>
+         <body id={styles.borda}>
             
-        <h3 className={styles.estiloh3}>O QUE DOAR</h3>
-        <h4 className={styles.estiloh4}>Escolha o item com qual você deseja ajudar ou receber ajuda</h4>
+        <h3 className={styles.estiloh3}>ONGs</h3>
+        <h4 className={styles.estiloh4}>Encontrea ONG que você deseja</h4>
 
             <div className={styles.container}>
 
             <div className={styles.content}>
 
-            <div className={styles.content1}>
-                <div className={styles.contentImg}>
-                    <img className={styles.img} src={garf} alt="garfield" />
+                <div className={styles.content1}>
+                    <div className={styles.contentImg}>
+                        <img className={styles.img} src={garf} alt="garfield" />
+                    </div>
+                        <div className={styles.contentText}>
+                        <h3>ABEPARES</h3>
+                        <p></p>
+                        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel malesuada tellus. Mauris ornare maximus libero, ut tempor odio tempus eu. Nam sed risus rhoncus, hendrerit ex nec, mattis ante. Morbi accumsan ac risus a scelerisque. Maecenas </h4>
+                        </div>
                 </div>
-                <div className={styles.contenttext}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel malesuada tellus. Mauris ornare maximus libero, ut tempor odio tempus eu. Nam sed risus rhoncus, hendrerit ex nec, mattis ante. Morbi accumsan ac risus a scelerisque. Maecenas quis ligula vel diam finibus cursus nec eu elit. Donec ex tellus, facilisis vitae mi eget, malesuada malesuada diam.
-                </div>
-            </div>
             </div>
 
-            </div>
+                <p className={styles.espaco}></p>
 
+                <div className={styles.content}>
+
+                <div className={styles.content1}>
+                    <div className={styles.contentImg}>
+                        <img className={styles.img} src={garf} alt="garfield" />
+                    </div>
+                        <div className={styles.contentText}>
+                        <h3>ABEPARES</h3>
+                        <p></p>
+                        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel malesuada tellus. Mauris ornare maximus libero, ut tempor odio tempus eu. Nam sed risus rhoncus, hendrerit ex nec, mattis ante. Morbi accumsan ac risus a scelerisque. Maecenas quis ligula vel diam finibus cursus nec eu elit. Donec ex tellus, facilisis vitae mi eget, malesuada malesuada diam.</h4>
+                        </div>
+                </div>
+                </div>
+
+            </div>
          </body>
         </>
     )
